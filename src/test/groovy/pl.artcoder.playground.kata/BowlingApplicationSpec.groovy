41a661542fabd6c0cc3d6a1cac49267deef6560c
Arek Sojda <arkadiusz.sojda@gmail.com>
2017-10-26 16:32:48 +0200

    Initial kotlin project with spock framework support configured

---
+++ src/test/groovy/pl.artcoder.playground.kata/BowlingApplicationSpec.groovy
@@ -0,0 +1,13 @@
+package pl.artcoder.playground.kata
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class BowlingApplicationSpec extends Specification {
+
+    def "should load context"() {
+
+    }
+
+}
